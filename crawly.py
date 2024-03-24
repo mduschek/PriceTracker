0e0f1c8da1098fb1e0f224b047dff42a47680507
mduschek <duschek54321@gmail.com>
2024-03-24 17:17:51 +0100

    changes to run headless

--- crawly.py
+++ crawly.py
@@ -84,7 +84,7 @@ class Crawly:
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.3"
 
         firefox_options = webdriver.FirefoxOptions()
-        # firefox_options.add_argument('--headless')
+        firefox_options.add_argument('--headless')
         firefox_options.add_argument(f'user-agent={user_agent}')
         firefox_options.add_argument('--disable-gpu')
         driver = webdriver.Firefox(options=firefox_options)
